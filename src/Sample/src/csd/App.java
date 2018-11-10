becb36f0892e885f5fdd160dadc048841514bf2f
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:46:58 +0300

    Aşağıdaki işlem aşağıdaki gibi de yapılabilir

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,35 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Metot çağrısında argüman olarak verilen ifadeler önce hesaplanır. İfadelerden elde edilen değerler ile
-	metot çağrılır
+	Yukarıdaki işlem aşağıdaki gibi de yapılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        Sample.foo(Sample.bar());
+        int result = Sample.bar();
+
+        Sample.foo(result);
     }
 }
 
 
 class Sample {
-
-    //argümanı ekrana basıyor.
     public static void foo(int a)
     {
         System.out.println(a);
     }
 
-    //kullanıcıdan sayı alıp karesinin alıyormuş
     public static int bar()
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.println("val1?");
-        int val1 = Integer.parseInt(kb.nextLine());
+        System.out.println("Bir sayı giriniz");
+        int val = Integer.parseInt(kb.nextLine()); //enter bekliyor cpu
 
-        int result = val1 * val1;
-
-        kb.close();
-        return result;
+        return val * val;
     }
 }
+
