1535974755d5d037a186972769818b6a44056e54
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:40:33 +0300

    Metot çağrılırken parametrelere aktarılan değerlere argüman denir.

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,13 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Parametre değişkenleri metot başında bildirilen yerel değişkenler gibidir.
-	Yani parametre değişkenlerinin faaliyet alanı bildirildikleri metot bloğunun tamamıdır
+	Metot çağrılırken parametrelere aktarılan değerlere argüman denir. Metot kaç parametreli ise
+	o kadar sayıda argümanla çağrılmalıdır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
+        int x = 10;
+        double y = 3.4;
 
+        Sample.foo(x,  y);
     }
 }
 
@@ -15,12 +18,7 @@ class App {
 class Sample {
     public static void foo(int a, double b)
     {
-        //...
-
-    }
-
-    public static void bar(int a, double b)
-    {
-
+        System.out.println(a);
+        System.out.println(b);
     }
 }
