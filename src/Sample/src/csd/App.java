3f3bfcb234b828df3e844bbb870c6b8a4e680b58
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 04:40:44 +0300

    Değişken Bildirimi  <tür> <isim> 3 yerde yapılabilir

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,32 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-   Çağıran metodun(foo), ait olduğu sınıf(Sample) ile çağrılan(bar) aynı sınıfta ise çağırma sırasında paket ismş
-   yazılmayabilir size kalmış. Hatta sınıf ismi bile yazmasan olur aynı sınıftlar.
+	Değişken bildirimi (declaration):
+	<tür> <isim>;
 ----------------------------------------------------------------------------------------------------------------------*/
-package csd;
 
-//public erişim belirleyici
-public class App {
-    public static void main(String[] args)
-    {
-
-    }
-}
-
-class Sample {
-    public static void foo()
-    {
-        System.out.println("sample.foo");
-
-        csd.Mample.bar();
-        //is equivalent, paket ismi gerek olmadı çağıran foo() ile çağrılan bar() aynı paketteler ama farklı sınıftalar.
-        Mample.bar();
-    }
-}
-
-
-class Mample {
-    public static void bar()
-    {
-        System.out.println("Mample.bar");
-    }
-}
+/*----------------------------------------------------------------------------------------------------------------------
+	Java' da 3(üç) yerde değişken bildirimi yapılabilir:
+	1. Yerel değişkenler (Local variables)
+	2. Metotların parametre değişkenleri
+	3. Referans türlerinin veri elemanları
+----------------------------------------------------------------------------------------------------------------------*/
