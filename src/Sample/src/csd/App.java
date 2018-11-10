003a229d6825735bc48d2461737f63449f060def
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 04:07:22 +0300

    Aynı Dosyada Birden Fazla Sınıf Bildirim ve public ise dosya ismiyle aynı aksi durumda farketmemesi

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,21 +1,29 @@
-
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf bildiriminin genel biçimi:
+    Bir java dosyasında birden fazla sınıf bildirimi yapılabilir.
+    Burada 3 tane sınıf bildirimi yapılmıştır (App, Test, Sample, sınıf bildirimin genel biçimini hatırlayın.)
 
-	[erişim belirleyici] class <sınıf ismi> {
-	    //...
-	}
+    Buradaki ayrıntı şu:
+        Bunlardan sadece biri [erişim belirleyici] olarak public olabilir. Örneğin App public ise diğerleri public olamaz
+        [erişim belirleyici] olarak public bildirilmişşe, bu sınıfın ismi ile dosyanın ismi aynı olmalı.
 
-	Burada [] konmasada olur anlamında <sen içini doldur>
-
+        Aksi durumda sınıflardan hiçbiri public erişim belirleyici ile bildirilmemişse. Dosyanın ismi sınıf isimlerinden
+        bambaşka olabilir.
 
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
+
+
+class App {
+    public static void main(String[] args)
+    {
+        System.out.println("Merhaba Java");
+    }
+}
 
 class Test {
     //...
 }
 
-class Date {
+class Sample {
     //...
 }
