3b580c3abce2a08fbdcaa9bb79498719655d2524
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 03:51:30 +0300

    Java' da temel kod yazım kuralları

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,52 +1,42 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Atomlar
-	1. Keywords, reserved words (anahtar sözcük) (a.s.)
-	2. Identifiers, variables (değişken) (d)
-	3. Literals, constants (sabitler) (s)
-	4. Operators (operatörler) (o)
-	5. String ler (str)
-	6. Delimiters, punctuators (ayraçlar) (a)
+	Java' da temel kod yazım kuralları:
+	1. Atomlar arasında istenildiği kadar boşluk (whitespace) bırakılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------------------------------------------------
-	Merhaba Java programının atomları:
-	package			a.s
-	csd				d
-	;				a
-	class			a.s.
-	App				d
-	{}				a
-	public			a.s.
-	static			a.s.
-	void			a.s.
-	main			d
-	()				a
-	String			d
-	[]				a
-	args			d
-	System			d
-	.				o
-	out				d
-	println			d
-	()				o
-	"Marhaba Java"	s
-----------------------------------------------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------------------------------------------------
-	Java da yorum satırları (comment line)
-----------------------------------------------------------------------------------------------------------------------*/
-
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        // bu kısım derleyici tarafından görünmez
-        System.out.println("Merhaba Java"); // bu kısım da derleyici tarafından görünmez
-		/*
-			Burası da
+        System.
 
-			görünmez
-		*/
+
+
+
+                out.
+
+
+
+
+
+                println								("Merhaba Java");
+
+
+
+
     }
+
 }
+
+/*----------------------------------------------------------------------------------------------------------------------
+	2. Atomlar istenildiği kadar bitişik yazılabilir. Ancak anahtar sözcükler ve değişken atomlar bitişik
+	yazılamaz
+----------------------------------------------------------------------------------------------------------------------*/
+//package csd;class App{public static void main(String[]args){System.out.println("Merhaba Java");}}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	2. Atomlar istenildiği kadar bitişik yazılabilir. Ancak anahtar sözcükler ve değişken atomlar bitişik
+	yazılamaz
+----------------------------------------------------------------------------------------------------------------------*/
+//package csd;classApp{public static void main(String[]args){System.out.println("Merhaba Java");}} //error
+
+
