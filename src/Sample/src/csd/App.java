c112ba5b495e8c80a0010285a9a1100e62fecfbe
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:11:20 +0300

    Sınıf Çalışması: Klavyeden girilen 2 sayının toplamı, çarpımı, çıkarımı, bölümü, modu...(kalıp kullanımı, result kullanımı)

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,43 +1,62 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Klavyeden Long, Double türden bir değer isteme kalıbı
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
+	Sınıf Çalışması: Klavyeden iki tane int türden sayı isteyen ve bu sayıların toplamını, farkını  ve çarpımını
+	ekrana yazdıran programı yazınız
+----------------------------------------------------------------------------------------------------------------------*/package csd;
 class App {
+
     public static void main(String [] args)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
 
-        System.out.println("Bir sayı giriniz");
-        long val = Long.parseLong(kb.nextLine());
+        System.out.println("val1?");
+        int val1 = Integer.parseInt(kb.nextLine());
 
-        System.out.println(val * val);
+        System.out.println("val2?");
+        int val2 = Integer.parseInt(kb.nextLine());
+
+        System.out.printf("%d + %d = %d%n", val1, val2, val1 + val2);
+        System.out.printf("%d - %d = %d%n", val1, val2, val1 - val2);
+        System.out.printf("%d * %d = %d%n", val1, val2, val1 * val2);
+        System.out.printf("%d / %d = %d%n", val1, val2, val1 / val2);
 
         kb.close();
     }
 }
 
 
-class Sample {
-    public static void foo()
+/*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Klavyeden iki tane int türden sayı isteyen ve bu sayıların toplamını, farkını  ve çarpımını
+	ekrana yazdıran programı yazınız
+----------------------------------------------------------------------------------------------------------------------*/
+
+class App1 {
+    public static void main(String [] args)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.println("sayi?");
-        double val = Double.parseDouble(kb.nextLine());
-        System.out.println(val * val);
+
+        System.out.println("Birinci sayıyı giriniz");
+        int val1 = Integer.parseInt(kb.nextLine());
+
+        System.out.println("İkinci sayıyı giriniz");
+        int val2 = Integer.parseInt(kb.nextLine());
+
+        //result mantığı ta buradan başlıyor.
+
+        int result = val1 + val2;
+
+        System.out.println(result);
+
+        result = val1 * val2;
+
+        System.out.println(result);
+
+        result = val1 - val2;
+
+        System.out.println(result);
+
+
         kb.close();
     }
-
 }
 
-class Mample {
-    public static void foo()
-    {
-        java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.println("sayi?");
-        long val = Long.parseLong(kb.nextLine());
-        System.out.println(val * val);
-        kb.close();
-    }
 
-}
