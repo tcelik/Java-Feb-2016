44fa947461263fc71bed678b76e8648e3665a6dc
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 04:17:41 +0300

    Metot Çağırması GenelBiçimi

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,12 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Metot bildiriminin genel biçimi:
+    Sınıf bildirdik.
+    Metot bildirdik
+    Metodlarımızı nasıl çağırıyoruz.
 
-    [erişim belirleyici] [static] <geri dönüş türü> <isim> ([parametreler])
-    {
-        //...
-    }
+    [paket ismi].[sınıf ismi].<metot ismi>([argümanlar])
 
-    Erişim belirleyici şunlar olabilir: public, private, protected, no modifier(hiç bir şey yazmama durumu)
+    Metotları paket isminden başlarak çağırıyoruz aslında.  
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -15,23 +14,24 @@ public class App {
     public static void main(String[] args)
     {
         System.out.println("Merhaba Java");
+
     }
 }
 
-//no-modifier erişim belirleci
+
 class Sample {
 
-    //public bir metot bildirimi
+
     public static void foo()
     {
-        //...
+
         System.out.println("foo");
     }
 
 
     public static void bar()
     {
-        //...
+
         System.out.println("bar");
     }
 }
