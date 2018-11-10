6b996534b3bef015b0a16a0789f44293caa09f1d
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 04:51:21 +0300

    Faaliyet alanı(Scope Kavramı)

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,29 +1,128 @@
-
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir değişkene bildirimi noktasında atanan değere ilkdeğer verme (initilization) denir.
+	Java da aynı faaliyet alanı içerisinde aynı isimde birden fazla değişken bildirimi yapılamaz
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        csd.Sample.foo();
+        Sample.foo();
     }
 }
 
 class Sample {
     public static void foo()
     {
-        int a = 45; //initialization (ilk değer verme)
+        int a;
 
 
-        a = 34; //atama
+        //...
+        double a;
 
 
-        a = a * 2;
 
+    }
+    //...
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	Java da kapsayan blokta bildirilmiş bir değişken ismi kapsanan bloklarda yeni bir değişken
+	bildirimi olarak kullanılamaz
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        Sample.foo();
+    }
+}
+
+class Sample {
+    public static void foo()
+    {
+        int a;
+
+
+        //...
+        {
+            double a; //error
+
+            //...
+        }
+
+
+
+    }
+    //...
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	Ayrık bloklar içerisinde aynı isimde yerel değişkenler olabilir
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        Sample.foo();
+    }
+}
+
+class Sample {
+    public static void foo()
+    {
+
+        {
+            int a;
+
+        }
+
+
+        //...
+        {
+            int a;
+
+            //...
+        }
+
+
+
+    }
+    //...
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	Aşağıdaki bildirimler geçerlidir. İç bloğun içerisindeki değişkenin faaliyet alanı bittikten sonra
+	aynı isimde diğer değişken bildirilmiştir
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        Sample.foo();
+    }
+}
+
+class Sample {
+    public static void foo()
+    {
+
+        //...
+        {
+            int a = 34;
+
+            //...
+
+            System.out.println(a);
+        }
+
+
+        int a = 67;
 
         System.out.println(a);
+
     }
     //...
 }
