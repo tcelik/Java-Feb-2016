5bfcce807b90e508b7b344a647b0e5ab2ed9494e
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:00:47 +0300

    Klavyeden int türden bir değer isteme kalıbı

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,103 +1,42 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	03.04.2018
-	Değişken isimleri rakam karakterleri ile başlayamaz
+	Klavyeden int türden bir değer isteme kalıbı
+
+
+	metotlara nasıl ulaşıyorduk
+    paketismi.sınıfismi.metotismi
+
+    sınıflara
+    paketismi.sınıfismi ile ulaşacağız.
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        int 1number; //error
-    }
-}
+        java.util.Scanner kb = new java.util.Scanner(System.in);
 
-/*----------------------------------------------------------------------------------------------------------------------
-	Değişken isimleri başlayabileceği herhangi bir karakter ile başlayıp istediği kadar rakam karakteri
-	içerebilir
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
+        System.out.println("Bir sayı giriniz");
+        int val = Integer.parseInt(kb.nextLine());
 
-class App {
-    public static void main(String [] args)
-    {
-        int a1234;
+        System.out.println(val * val);
 
-        a1234 = 23;
-
-        System.out.println(a1234);
-    }
-}
-
-/*----------------------------------------------------------------------------------------------------------------------
-	Değişken isimleri alttire (underscore) karakterini içerebilir hatta başlatılabilir
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
-class App {
-    public static void main(String [] args)
-    {
-        int _1234;
-
-        _1234 = 23;
-
-        System.out.println(_1234);
-    }
-}
-
-/*----------------------------------------------------------------------------------------------------------------------
-	Değişken isimleri boşluk karakteri (whitespace) içeremez
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
-class App {
-    public static void main(String [] args)
-    {
-        int number of students; //error
+        kb.close();
 
 
     }
 }
 
-/*----------------------------------------------------------------------------------------------------------------------
-	Alttire nin tek başına değişken ismi olması Java 9 ile birlikte geçersizdir
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
 
-class App {
-    public static void main(String [] args)
+class Sample {
+    public static void foo()
     {
-        int _ = 10; //error: Since Java 9
+        java.util.Scanner kb = new java.util.Scanner(System.in);
 
-        System.out.println(_);
-    }
-}
+        System.out.println("Sayı?");
 
-/*----------------------------------------------------------------------------------------------------------------------
-	$ karakteri değişken isimlendirmede kullanılabilir. Fakat programcılar
-	tarafından tercih edilmez
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
-class App {
-    public static void main(String [] args)
-    {
-        int numberOf$s = 10;
-
-        System.out.println(numberOf$s);
-    }
-}
-
-/*----------------------------------------------------------------------------------------------------------------------
-	Değişken isimleri UNICODE karakterler içerebilir. Yani Türkçe karakterler
-	değişken isimlendirmede kullanılabilmektedir
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
-class App {
-    public static void main(String [] args)
-    {
-        int sayı = 10;
-
-        System.out.println(sayı);
+        //kb.nextLine() enter bekleyecek. tıklanınca girilen sayıyı dönecek.
+        int val = Integer.parseInt(kb.nextLine());
+        System.out.println(val * val);
+        kb.close();
     }
 }
