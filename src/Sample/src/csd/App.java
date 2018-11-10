34d66bb197ceb0da05e81ed41715c552583c439b
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:49:41 +0300

    Bir metoda argüman olarak bir  metodun geri dönüş değeri geçilebilir

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,31 +1,39 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Yukarıdaki işlem aşağıdaki gibi de yapılabilir
+	Bir metoda argüman olarak bir  metodun geri dönüş değeri geçilebilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        int result = Sample.bar();
+        int x = 4;
 
-        Sample.foo(result);
+        //önce hesaplanır.
+        Sample.foo(Sample.bar(Sample.tar(x * x)));
     }
 }
 
 
 class Sample {
+    //ekrana bas
     public static void foo(int a)
     {
         System.out.println(a);
     }
 
-    public static int bar()
+    //ver bana bir sayı, kullanıcdan aldığım değerle çarpim dönim.
+    public static int bar(int a)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
         System.out.println("Bir sayı giriniz");
-        int val = Integer.parseInt(kb.nextLine()); //enter bekliyor cpu
+        int val = Integer.parseInt(kb.nextLine());
 
-        return val * val;
+        return val * a;
+    }
+
+    //ver bana bi sayı 2 ile çarpim
+    public static int tar(int a)
+    {
+        return a * 2;
     }
 }
-
