9f44943ef93f00e7d44936dd407e12cca777b52c
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 05:53:36 +0300

    printf ile % karakteri %% ile yazdırılabilir

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,6 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	printf metodu ile ekrana formatlı yazdırma yapılabilir. Formatlı yazdırma için yer tutucu (placeholder)
-	veya format karakteri denilen özel karakterler % karakteri ile kullanılır.
+	printf ile % karakteri %% ile yazdırılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -8,9 +7,10 @@ class App {
     public static void main(String [] args)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.println("Hava bugün kaç derece");
-        int degree = Integer.parseInt(kb.nextLine());
+        System.out.println("Havadaki nem oranını giriniz");
+        int ratio = Integer.parseInt(kb.nextLine()); //3 girdi, sen %3 demek istiyorsun
 
-        System.out.printf("Bugün hava %d derece", degree);
+        System.out.printf("Havada %%%d nem var%n", ratio);  //                 %%(tek yüzde için) - %d (3 için)
+
     }
 }
