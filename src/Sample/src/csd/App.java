8359b5a622ef740c5952999b900fc4713b852508
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-10 03:55:21 +0300

    Paket Bildirimi

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,42 +1,9 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	Java' da temel kod yazım kuralları:
-	1. Atomlar arasında istenildiği kadar boşluk (whitespace) bırakılabilir
-----------------------------------------------------------------------------------------------------------------------*/
-package csd;
-
-class App {
-    public static void main(String [] args)
-    {
-        System.
-
-
-
-
-                out.
-
-
-
-
-
-                println								("Merhaba Java");
-
-
-
-
-    }
-
-}
 
 /*----------------------------------------------------------------------------------------------------------------------
-	2. Atomlar istenildiği kadar bitişik yazılabilir. Ancak anahtar sözcükler ve değişken atomlar bitişik
-	yazılamaz
+	Paket bildiriminin genel biçimi:
+
+	package <isim>;
+	package csd;
+
+	Paketlerin içerise sınıflar koyacağız.
 ----------------------------------------------------------------------------------------------------------------------*/
-//package csd;class App{public static void main(String[]args){System.out.println("Merhaba Java");}}
-
-/*----------------------------------------------------------------------------------------------------------------------
-	2. Atomlar istenildiği kadar bitişik yazılabilir. Ancak anahtar sözcükler ve değişken atomlar bitişik
-	yazılamaz
-----------------------------------------------------------------------------------------------------------------------*/
-//package csd;classApp{public static void main(String[]args){System.out.println("Merhaba Java");}} //error olur
-
-
