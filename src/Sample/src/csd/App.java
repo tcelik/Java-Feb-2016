9a48a037550f1779f3d93703b050a1e8d5304c8d
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-11 23:42:39 +0300

    Math sınıfının yuvarlama yapan metotları

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,7 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 Sınıf Çalışması: Parametresi ile aldığı (x1, y1) ve (x2, y2) nokatalarına ilişkin iki nokta arasındaki
-	 uzaklığı döndüren distance isimli metodu yazınız ve kalvyeden alınan değerler ile çağırarak
-	 test ediniz. Distance metodunu Util isimli bir sınıf içerisinde yazınız
+	 Math sınıfının yuvarlama yapan metotları
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -9,31 +7,14 @@ class App {
     public static void main(String [] args)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.print("x1?");
-        double x1 = Double.parseDouble(kb.nextLine());
+        System.out.println("Bir sayı giriniz");
+        double val = Double.parseDouble(kb.nextLine());
 
-        System.out.print("y1?");
-        double y1 = Double.parseDouble(kb.nextLine());
+        System.out.printf("round(%.2f)=%d%n", val, Math.round(val)); //10.5 ve büyükler : 11 e, 10.5'dan küçükler 10'a
+        System.out.printf("rint(%.2f)=%f%n", val, Math.rint(val));//10.5 : 10.00 diğerli raund
+        System.out.printf("ceil(%.2f)=%f%n", val, Math.ceil(val)); //yukarıya  10.9 : 11'a
+        System.out.printf("floor(%.2f)=%f%n", val, Math.floor(val)); //aşağıda 10.9 : 10'a
 
-        System.out.print("x2?");
-        double x2 = Double.parseDouble(kb.nextLine());
-
-        System.out.print("y2?");
-        double y2 = Double.parseDouble(kb.nextLine());
-
-        double dist = Util.distance(x1, y1, x2, y2);
-
-        System.out.printf("dist[(%.2f, %.2f), (%.2f, %.2f)] = %.3f%n", x1, y1, x2, y2, dist);
         kb.close();
-
-    }
-}
-
-class Util {
-    //sqrt öğrenmiştik.
-    public static double distance(double x1, double y1, double x2, double y2)
-    {
-        //return Math.sqrt(Math.abs(x2 - x1) * Math.abs(x2 - x1) + Math.abs(y2 - y1) * Math.abs(y2 - y1));
-        return Math.sqrt((x2 - x1) * (x2 - x2) + (y2 - y1) * (y2 - y1));
     }
 }
