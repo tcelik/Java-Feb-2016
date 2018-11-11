55b2de5927bb2968997f109b7eff5e212d65ac15
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-12 00:01:57 +0300

    ters bölü karakeri çift \\ olarak kullanılır. System.out.println("\\\\192.168.2.101\\testdb.db"); 4 ters bölü 2 ters bölü yapıyro bu durumda

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,12 +1,13 @@
+
 /*----------------------------------------------------------------------------------------------------------------------
-	İki tırnak içerisinde escape sequence karakterler kullanılabilir. Bu durumda escape sequence karakter hangi
-	anlamdaysa o iş yapılır
+	İki tırnak içerisinde ters bölüden sonra escape sequence karakter gelmelidir.
+	Aşağıdaki örnekte \\192.168.2.101\testdb.db yazısı ekrana çıkmaktadır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
     public static void main(String [] args)
     {
-        System.out.println("C:\test\names.txt");
+        System.out.println("\\\\192.168.2.101\\testdb.db");
     }
 }
