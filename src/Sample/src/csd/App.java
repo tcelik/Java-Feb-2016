f035dc98872b68c864dc6dc8946798ff359a93a7
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-11 23:51:38 +0300

    0 girersem exception oluşur ve program çöker

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,8 +1,6 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 Sınıf Çalışması: Util sınıfı içerisinde bir üçgenin üç kenarını parametre olarak alıp alanını döndüren
-	 getTriangleArea ve çevresini döndüren getTriangleCircumference metotlarını yazınız ve metotları
-	 klavyeden girilen değerler ile çağırarak test ediniz.
-	 Not: Kenarların üçgen koşullarını sağlayıp sağlamadığı test edilmeyecektir
+	12.04.2018
+	Aşağıdaki örnekte klavyeden girilen b sayısı için sıfır değeri exception oluşmasına yol açar
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -10,29 +8,15 @@ class App {
     public static void main(String [] args)
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
-        System.out.println("a? b? c?");
-        double a = Double.parseDouble(kb.next());
-        double b = Double.parseDouble(kb.next());
-        double c = Double.parseDouble(kb.next());
+        System.out.println("Birinci sayıyı giriniz");
+        int a = Integer.parseInt(kb.nextLine());
 
-        System.out.println(Util.getTriangleArea(a, b, c));
+        System.out.println("İkinci sayıyı giriniz");
+        int b = Integer.parseInt(kb.nextLine());
+
+        System.out.println(a / b);
+        System.out.println("akış buraya gelmez");
 
         kb.close();
-    }
-}
-
-class Util {
-    public static double getTriangleArea(double a, double b, double c)
-    {
-        //...
-        double u = getTriangleCircumference(a, b, c) / 2;
-
-        return Math.sqrt(u * (u - a) * (u - b) * (u - c));
-    }
-
-    public static double getTriangleCircumference(double a, double b, double c)
-    {
-        //...
-        return a + b + c;
     }
 }
