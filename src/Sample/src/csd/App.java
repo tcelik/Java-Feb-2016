0c2a36c7ec8c506ccbe75ec8acde8d61bfee2b03
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-11 23:03:13 +0300

    Math.sqrt()

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -6,13 +6,13 @@ class App {
     {
         java.util.Scanner kb = new java.util.Scanner(System.in);
         System.out.println("val1?");
-        int val1 = Integer.parseInt(kb.nextLine());
+        double val1 = Double.parseDouble(kb.nextLine());
 
-        int result = Math.abs(val1);
+        double result = Math.sqrt(val1);
 
-        //System.out.printf("|%d| = %d%n", val1, result);
+        System.out.printf("sqrt(%.2f) = %.2f%n", val1, result);
 
-        String fmt = "|%d|=%d%n";
+        String fmt = "sqrt(%.6f) = %.6f";
         System.out.printf(fmt, val1, result);
     }
 }
