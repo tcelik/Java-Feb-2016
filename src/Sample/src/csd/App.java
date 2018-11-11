2c4c822aada691940b37072ef9d9c9f8d02f2ecc
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2018-11-12 01:10:19 +0300

    ++ ve -- operatörlerinin karışık kullanımları

--- src/Sample/src/csd/App.java
+++ src/Sample/src/csd/App.java
@@ -1,6 +1,5 @@
-
 /*----------------------------------------------------------------------------------------------------------------------
-	++ operatörünün postfix kullanımının ürettiği değer (işleme giren değer) artırılMAmış değerdir
+	++ ve -- operatörlerinin karışık kullanımları
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -8,10 +7,61 @@ class App {
     public static void main(String [] args)
     {
         int a = 10;
+        int b = a++ + --a;
 
-        int b = a++;
+        System.out.printf("a=%d%n", a); // a=10
+        System.out.printf("b=%d%n", b); // b=20
+    }
+}
 
-        System.out.printf("b=%d%n", b);
-        System.out.printf("a=%d%n", a);
+/*----------------------------------------------------------------------------------------------------------------------
+	++ ve -- operatörlerinin karışık kullanımları
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        int a = 10;
+        int b = a++ + --a + a++;
+
+        System.out.printf("a=%d%n", a); // a=11
+        System.out.printf("b=%d%n", b); // b=30
+    }
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	++ ve -- operatörlerinin karışık kullanımları
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        int a = 10;
+        int b = a++ + --a + ++a;
+
+        System.out.printf("a=%d%n", a); // a=11
+        System.out.printf("b=%d%n", b); // b=31
+    }
+}
+
+/*----------------------------------------------------------------------------------------------------------------------
+	Aşağıdaki örnekte derleyici anlamlı en uzun atomu alarak işlem yaptığı için ayırma
+	a++ + b
+	biçimindedir
+----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+    public static void main(String [] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = a+++b; //a++ + b
+
+        System.out.printf("a=%d%n", a); // a=11
+        System.out.printf("b=%d%n", b); // b=20
+        System.out.printf("c=%d%n", c); // b=30
     }
 }
